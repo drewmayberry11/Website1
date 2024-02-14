7c7f1b693bc57e07aae06404e0fbb800eed53765
Drew Mayberry <76681966+drewmayberry11@users.noreply.github.com>
2024-02-14 11:06:27 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const menuItems = document.querySelectorAll('.menu-bar ul li a');
 // Add event listeners to each menu item
 menuItems.forEach(menuItem => {
   menuItem.addEventListener('mouseover', () => {
-    menuItem.style.color = 'red';
+    menuItem.style.color = 'red'; // Change text color to red on mouseover
   });
 
   menuItem.addEventListener('mouseout', () => {
-    menuItem.style.color = 'white';
+    menuItem.style.color = 'white'; // Revert text color to white on mouseout
   });
 });
